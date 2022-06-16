3e21bcc946acc2ddfc4d7dab350c77de84193ad9
Harshvardhan Ojha <ojhaharsh7@gmail.com>
2022-06-17 01:49:03 +0530

    new: main file added to run code

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        String data = readLine();
+        System.out.println(data);
+    }
+
+    static String readLine() throws Exception {
+        return br.readLine().trim();
+    }
+}
